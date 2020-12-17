310775995182273b54cbd878740738fabd8fe6a2
Luca Meneghini <lucamene98@outlook.it>
2020-12-17 23:51:13 +0100

    :heavy_plus_sign: Add basic dependencies

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: 8,
+        },
+      },
+    ],
+    "@babel/preset-react",
+  ],
+  ignore: ["**/node_modules/**/*", "**/public_html/**/build/**/*"],
+  styledComponents: {
+    pure: true,
+  },
+};
